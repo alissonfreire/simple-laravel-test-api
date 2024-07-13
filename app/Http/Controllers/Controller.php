db728ee758ee6079372a2a8318d9fe1fe97afc88
Alisson Freire <alissonfreire2@gmail.com>
2024-07-13 00:49:39 -0300

    feat: added swagger attributes in AuthController and generate api-docs.file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    title: 'Simple Todo Rest API',
+    version: '1.0.0',
+    description: "An API for managing user's todos"
+)]
+#[OA\Server(
+    url: 'http://localhost:8000',
+    description: 'Simple Todo Rest API'
+)]
+
+#[OA\SecurityScheme(type: 'http', scheme: 'bearer', securityScheme: 'bearerAuth')]
 abstract class Controller
 {
     //
